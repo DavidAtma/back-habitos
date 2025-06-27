22a45e101c35c455585b36bd02234d001170161f
Monty <allccavalenzuela@gmail.com>
2025-06-27 18:21:04 -0500

    Update usuario.ts

--- src/entities/usuario.ts
+++ src/entities/usuario.ts
@@ -1,29 +1,39 @@
-import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
-import { Rol } from "./rol";
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Rol } from './rol';
 
 @Entity('Usuarios')
 export class Usuario {
+  @PrimaryGeneratedColumn({ name: 'id_usuario' })
+  idUsuario: number;
 
-    @PrimaryGeneratedColumn({ name: 'id_usuario' })
-    idUsuario: number;
-    @ManyToOne(() => Rol, (rol) => rol.idRol)
-    @JoinColumn({ name: 'id_rol' })
-    rol: Rol
-    @Column({ name: 'nombre', type: 'varchar', length: 250 })
-    nombre: string;
-    @Column({ name: 'apellido_paterno', type: 'varchar', length: 250 })
-    apellidoPaterno: string;
-    @Column({ name: 'apellido_materno', type: 'varchar', length: 250 })
-    apellidoMaterno: string;
-    @Column({ name: 'correo', type: 'varchar', length: 100, unique: true })
-    correo: string;
-    @Column({ name: 'contrasena', type: 'varchar', length: 256 })
-    contrasena: string;
-    @Column({ name: 'foto_perfil', type: 'varchar', length: 1000 })
-    fotoPerfil: string;
-    @CreateDateColumn({ name: 'fecha_creacion', type: 'datetime' })
-    fechaCreacion: Date;
-    @Column({ name: 'estado_auditoria', type: 'bit', default: 1 })
-    estado: boolean;
-    
+  @ManyToOne(() => Rol, (rol) => rol.idRol)
+  @JoinColumn({ name: 'id_rol' })
+  rol: Rol;
+
+  @Column({ name: 'nombre', type: 'varchar', length: 250 })
+  nombre: string;
+
+  @Column({ name: 'apellido_paterno', type: 'varchar', length: 250 })
+  apellidoPaterno: string;
+
+  @Column({ name: 'apellido_materno', type: 'varchar', length: 250 })
+  apellidoMaterno: string;
+
+  @Column({ name: 'correo', type: 'varchar', length: 180, unique: true }) // ← actualizado a 180
+  correo: string;
+
+  @Column({ name: 'contrasena', type: 'varchar', length: 256 })
+  contrasena: string;
+
+  @Column({ name: 'fecha_nacimiento', type: 'date', nullable: true }) // ← agregado nuevo campo
+  fechaNacimiento: Date;
+
+  @Column({ name: 'foto_perfil', type: 'varchar', length: 1000 })
+  fotoPerfil: string;
+
+  @CreateDateColumn({ name: 'fecha_creacion', type: 'datetime' })
+  fechaCreacion: Date;
+
+  @Column({ name: 'estado_auditoria', type: 'bit', default: true })
+  estado: boolean;
 }
